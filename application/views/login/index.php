9f79a7e0fbf9c8d4b635ab0b1f599104fc212ca3
peteyCore14 <33260827+peteyCore14@users.noreply.github.com>
2020-10-03 12:18:06 +0700

    Update index.php

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Quick Admin</title>
+    <title>Anterin Si-OnDel Panel</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="<?= base_url(); ?>asset/node_modules/mdi/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="<?= base_url(); ?>asset/node_modules/simple-line-icons/css/simple-line-icons.css">
